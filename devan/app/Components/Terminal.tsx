4040fafbd35728ba8e18996703d516fd2ad10787
Dev Nandan <a.d.devnandan@gmail.com>
2025-01-11 12:47:13 +0530

    init

--- devan/app/Components/Terminal.tsx
+++ devan/app/Components/Terminal.tsx
@@ -23,3 +23,57 @@ export default function Terminal() {
     );
   }
   
+
+
+      // Todo.js
+    
+    // [...]
+    
+
+  // [..]
+
+    // File: Todo.js
+    
+    // [...]
+    
+    function Todo() {
+      const [tasks, setTasks] = useState([
+          {
+              title: "Grab some Pizza",
+              completed: true
+          },
+          {
+              title: "Do your workout",
+              completed: true
+          },
+          {
+              title: "Hangout with friends",
+              completed: false
+          }
+      ]);
+  
+      const addTask = title => {
+          const newTasks = [...tasks, { title, completed: false }];
+          setTasks(newTasks);
+      };
+  
+      return (
+          <div className="todo-container">
+              <div className="header">TODO - ITEMS</div>
+              <div className="tasks">
+                  {tasks.map((task, index) => (
+                      <Task
+                          task={task}
+                          index={index}
+                          key={index}
+                      />
+                  ))}
+              </div>
+              <div className="create-task" >
+                  <CreateTask addTask={addTask} />
+              </div>
+          </div>
+      );
+  }
+  
+  // [..]
